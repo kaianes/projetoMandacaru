f8e1ef62e95415d5169bfa4bb30d1b3470e1f32c
Kaiane <kaiane.cordeiro@sou.inteli.edu.br>
2023-02-15 15:50:49 -0300

    atualizacao1.0
    touch1

--- Codigos/principal.gd
+++ Codigos/principal.gd
@@ -17,3 +17,17 @@ func _on_botaoEsquerda_pressed():
 
 func _on_Button3_pressed():
 	Global.iniciou = true
+
+
+
+func _on_touchEsquerda_pressed():
+	Global.lista.append("esquerda")
+	
+func _on_touchDireita_pressed():
+	Global.lista.append("direita")
+
+func _on_touchCima_pressed():
+	Global.lista.append("cima")
+
+func _on_touchBaixo_pressed():
+	Global.lista.append("baixo")
